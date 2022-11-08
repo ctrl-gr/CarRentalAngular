c8e8774ab47aaa29561d92eb63ee88052a0a9fd7
ctrl-gr <giulia.redolfi@si2001.it>
2022-11-08 17:12:23 +0100

    is admin on navbar

---
+++ src/app/add-car/add-car.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Car} from "../car/car-config";
+import {CarService} from "../services/car.service";
+
+@Component({
+  selector: 'app-add-car',
+  templateUrl: './add-car.component.html',
+  styleUrls: ['./add-car.component.css']
+})
+export class AddCarComponent implements OnInit {
+
+  cars: Car[] = []
+  carform!: FormGroup;
+
+  constructor(private carService: CarService) { }
+
+  ngOnInit(): void {
+    this.carform = new FormGroup({
+      licensePlate: new FormControl(null, Validators.required),
+      manufacturer: new FormControl(null, Validators.required),
+      model: new FormControl(null, Validators.required),
+      type: new FormControl(null, Validators.required),
+      year: new FormControl(null, Validators.required),
+      seats: new FormControl(null, Validators.required)
+    })
+  }
+
+  addEditCar(carform: FormGroup) {
+    //id?
+    this.carService.addEditCar({
+      id: 1,
+      licensePlate: this.carform.value.licensePlate,
+      manufacturer: this.carform.value.manufacturer,
+      model: this.carform.value.model,
+      type: this.carform.value.type,
+      year: this.carform.value.year,
+      seats: this.carform.value.seats
+    })
+  }
+
+  onSubmit(form: FormGroup) {
+    this.addEditCar(this.carform)
+    console.log(this.carform)
+  }
+}
+
---
+++ src/app/services/car.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable, of} from "rxjs";
+import {catchError} from "rxjs/operators";
+import {Car} from "../car/car-config";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarService {
+
+  private carsUrl = 'api/cars';
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  getCars(): Observable<Car[]> {
+    return this.http.get<Car[]>(this.carsUrl)
+      .pipe(
+        catchError(this.handleError<Car[]>('getCars', []))
+      );
+  }
+
+
+  getCar(id: number): Observable<Car> {
+    const url = `${this.carsUrl}/${id}`;
+    return this.http.get<Car>(url).pipe(
+      catchError(this.handleError<Car>(`getCar id=${id}`))
+    );
+  }
+
+
+  addEditCar(car: Car): Observable<Car> {
+    return this.http.post<Car>(this.carsUrl, car, this.httpOptions).pipe(
+      catchError(this.handleError<Car>('addCar'))
+    );
+  }
+
+  deleteCar(id: number): Observable<Car> {
+    const url = `${this.carsUrl}/${id}`;
+
+    return this.http.delete<Car>(url, this.httpOptions).pipe(
+      catchError(this.handleError<Car>('deleteCar'))
+    );
+  }
+
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      console.error(error);
+
+      return of(result as T);
+    }
+  }
+
+}
