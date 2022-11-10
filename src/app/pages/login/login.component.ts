88269123c00375011ff1f39579375e65b718fd2e
ctrl-gr <giulia.redolfi@si2001.it>
2022-11-10 16:05:35 +0100

    first auth settings

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../../auth/auth.service";
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  signinForm: FormGroup;
+
+  constructor(
+    public fb: FormBuilder,
+    public authService: AuthService,
+    public router: Router
+  ) {
+    this.signinForm = this.fb.group({
+      username: [''],
+      password: [''],
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  loginUser() {
+    this.authService.signIn(this.signinForm.value);
+  }
+}
